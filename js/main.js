d3fb5b828e13b0b99bbf530881050ad3a43c3754
Aleri <aleri.chan@gmail.com>
2014-12-03 11:13:57 +0000

    Menu changes and form fix

--- js/main.js
+++ js/main.js
@@ -2,39 +2,40 @@
 function validateForm() {
 
 		var valid = false;
-		var fName = document.getElementById("firstname"),
-		lName = document.getElementById("surename"),
-		email = document.getElementById("email"),
-		checkbox = document.getElementById("terms"),
-		chbx = document.getElementById("hidden"),
-		btn = document.getElementById("form-submit");
+		var fName = document.getElementById("firstname");
+		var lName = document.getElementById("surename");
+		var email = document.getElementById("email");
+		var checkbox = document.getElementById("terms");
+		var chbx = document.getElementById("hidden");
+		var btn = document.getElementById("form-submit");
 	 	var regexName = /^[a-zA-Z ]{2,30}$/,
 	 		regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 		if(fName.value === "" || fName.value === null || !regexName.test(fName.value)) {
+			fName.value = "";
 			fName.placeholder = "Enter name";
-			fName.value = " ";
 			fName.focus();
 			return false;
-			
+
 		}
 
-			if(lName.value === "" || lName.value === null || !regexName.test(lName.value)) {
-				lName.placeholder = "Enter surename";
-				lName.focus();
-				return false;
-			}
-			if(email.value === "" || email.value === null || !regexEmail.test(email.value)) {
-				email.placeholder = "Enter a valid email";
-				email.focus();
-				email.value="";
-				return false;
-			}
-			
-			if(checkbox.checked === false) {
-				hidden.style.display = "block";
-				return false;
-			}
+		if(lName.value === "" || lName.value === null || !regexName.test(lName.value)) {
+			email.value = "";
+			lName.placeholder = "Enter surename";
+			lName.focus();
+			return false;
+		}
+		if(email.value === "" || email.value === null || !regexEmail.test(email.value)) {
+			email.value = "";
+			email.placeholder = "Enter a valid email";
+			email.focus();
+			return false;
+		}
+
+		if(checkbox.checked === false) {
+			hidden.style.display = "block";
+			return false;
+		}
 
 
 	return onSubmit(fName, lName, email, btn, checkbox);
@@ -51,4 +52,3 @@ function onSubmit(fName, lName, email, btn, checkbox) {
 
 	return false;
 }
-
